ff41611cb8f04143ed7625905d8aa6c163ddbd7b
Pablo0413 <71309446+Pablo0413@users.noreply.github.com>
2020-10-15 22:12:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-$(document).ready(function(){
-   
-    var homePos = $("#home").offset().top;
-    var aboutPos = $("#aboutme").offset().top;
-    var portfolioPos = $("#portfolio").offset().top;
-    var contactPos = $("#contactme").offset().top;
-    $("#home_page").click(function(){
-       $("html, body").animate({scrollTop:homePos -50}, 700, function(){
-          $(":focus").blur();
-       });
-       return false;
-    });
-    $("#about_page").click(function(){
-       $("html, body").animate({scrollTop:aboutPos -50}, 700, function(){
-          $(":focus").blur();
-       });
-       return false;
-    });
-    $(".portfolio_page").click(function(){
-       $("html, body").animate({scrollTop:portfolioPos -50}, 700, function(){
-          $(":focus").blur();
-       });
-       return false;
-    });
-    $("#contact_page").click(function(){
-       $("html, body").animate({scrollTop:contactPos -50}, 700, function(){
-          $(":focus").blur();
-       });
-       return false;
-    });
-    
- });
- 
+// select all the buttons
+const buttons = document.querySelectorAll('button');
+// select the <input type="text" class="display" disabled> element
+const display = document.querySelector('.display');
+
+// add eventListener to each button
+buttons.forEach(function(button) {
+  button.addEventListener('click', calculate);
+});
+
+// calculate function
+function calculate(event) {
+  // current clicked buttons value
+  const clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === '=') {
+    // check if the display is not empty then only do the calculation
+    if (display.value !== '') {
+      // calculate and show the answer to display
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === 'C') {
+    // clear everything on display
+    display.value = '';
+  } else {
+    // otherwise concatenate it to the display
+    display.value += clickedButtonValue;
+  }
+}
